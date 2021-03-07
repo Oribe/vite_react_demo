e3268a1c80845f60d0d95f8df9c03ea892afb1f7
Oribe <d_egoist@163.com>
2021-03-07 20:35:50 +0800

    准备修改图片选择有必选提示

--- src/component/ImgSelect/index.tsx
+++ src/component/ImgSelect/index.tsx
@@ -1,5 +1,5 @@
 import { Form, Space } from "antd";
-import React, { FC, memo } from "react";
+import React, { FC, memo, useMemo } from "react";
 import { ImageOptions } from "store/modules/form";
 import Image from "./image";
 import styles from "./index.module.scss";
@@ -12,12 +12,28 @@ const { Item } = Form;
 const ImgSelect: FC<Props> = (props) => {
   const { options } = props;
 
+  /**
+   * 获取关联字段
+   */
+  const dataIndexs = useMemo(() => options.map((item) => item.dataIndex), [
+    options,
+  ]);
+
   return (
     <Space className={styles.imgSelectRow} align="start" wrap size="middle">
       {options.map((option) => {
         return (
-          <Item key={option.dataIndex} name={option.dataIndex} noStyle={true}>
-            <Image key={option.label} {...option} />
+          <Item
+            key={option.dataIndex}
+            name={option.dataIndex}
+            noStyle={true}
+            dependencies={dataIndexs}
+          >
+            {({ getFieldsValue }) => {
+              const values = getFieldsValue(dataIndexs);
+              console.log("values", values);
+              return <Image key={option.label} {...option} />;
+            }}
           </Item>
         );
       })}
